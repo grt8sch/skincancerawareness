a196e0a3594f94145fbb5db2cb5ac7927728e90d
grt8sch <31548609+grt8sch@users.noreply.github.com>
2018-03-03 17:24:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+  //Display Content of Nav
+  function displayContent(){
+    $(".info-content:eq("+$(".nav-a").index(".selected")+")").css("opacity","1");
+  }
+  displayContent();
+  //Navigation
   $(".nav-a").click(function(){
     $(".nav-a").removeClass("selected");
     $(this).addClass("selected");
+    displayContent();
   });
 });
