3e35c0499a157aef70a32d1287160772ca2a38a6
grt8sch <31548609+grt8sch@users.noreply.github.com>
2018-03-03 17:27:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ $(document).ready(function(){
   function displayContent(){
     $(".info-content:eq("+$(".nav-a").index(".selected")+")").css("opacity","1");
   }
+  //Hide Content of Nav
+  function hideContent(){
+    $(".info-content:eq("+$(".nav-a").index(".selected")+")").css("opacity","0");
+  }
   displayContent();
   //Navigation
   $(".nav-a").click(function(){
+    hideContent();
     $(".nav-a").removeClass("selected");
     $(this).addClass("selected");
     displayContent();
